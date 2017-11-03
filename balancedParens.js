d418072929822d86f84310b069cdd68ee16919ad
Lyddem <lydiademi@gmail.com>
2017-11-02 19:24:44 -0700

    Completed

---
+++ balancedParens.js
@@ -0,0 +1,45 @@
+/*
+ * write a function that takes a string of text and returns true if
+ * the parentheses are balanced and false otherwise.
+ */
+
+ function  isBalanced (str) { //[{] current = '}'
+  var hash = {
+    '{':'}',
+    '[':']',
+    '(':')'
+  };
+
+  var final = [];
+
+  function isClosed(el) {
+    if(el === '}' || el === ']' || el === ')') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function isOpen(el) {
+    if(el === '{' || el === '[' || el === '(') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  for(var i = 0; i < str.length; i++) {
+    let el = str[i]
+    if(isOpen(el)) {
+      final.push(el);
+    }
+    if(isClosed(el)) {
+      if(hash[final[final.length-1]] === el) {
+        final.splice(final.length-1, 1)
+      }
+    }
+  }
+
+  return final.length === 0;
+
+}
