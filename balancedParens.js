f06b69fa9f3e72160d538f3469e14d270c1e0766
Lyddem <lydiademi@gmail.com>
2017-11-08 15:00:38 -0800

    Added alternate solution

--- balancedParens.js
+++ balancedParens.js
@@ -43,3 +43,48 @@
   return final.length === 0;
 
 }
+
+/* alternate solution
+
+function  isBalanced (str) {
+  var hash = {
+    '}':'{',
+    ']':'[',
+    ')':'('
+  };
+
+  var final = [];
+
+  function isClosed(el) {
+    if(el === '}' || el === ']' || el === ')') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function isOpen(el) {
+    if(el === '{' || el === '[' || el === '(') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  for(var i = 0; i < str.length; i++) {
+    let el = str[i];
+
+    if(isOpen(el)) {
+      final.push(el);
+    }
+
+    if(isClosed(el)){
+      if(final.pop() !== hash[el]) {
+        return false;
+      }
+    }
+  }
+  return final.length === 0;
+}
+
+*/
