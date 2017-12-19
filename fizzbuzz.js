76a2f2380f4843cc88be167d5dca8967f9e83146
Lyddem <lydiademi@gmail.com>
2017-12-18 19:19:19 -0800

    Completed

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+// see the Hint button if you're not sure how to start
+
+for(var i = 1; i <= 100; i++) {
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  }
+  else if(i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
+
+
+
